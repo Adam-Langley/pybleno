346c72a0563e329314ef07169f6b993a94c7c6e4
Adam-Langley <github.com@irisdesign.co.nz>
2017-11-28 21:35:53 +0000

    fixed pypi subpackage references

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
   name = 'pybleno',
-  packages = ['pybleno'], # this must be the same as the name above
+  packages = ['pybleno', 'pybleno/hci_socket',  'pybleno/hci_socket/BluetoothHCI'], # this must be the same as the name above
   version = '0.1',
   description = 'A direct port of the Bleno bluetooth LE peripheral role library to Python2/3',
   author = 'Adam Langley',
