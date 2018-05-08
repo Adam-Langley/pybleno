ea53158bf3c4d6540b853f577dab109a4eaf134f
Adam Langley <github.com@irisdesign.co.nz>
2018-05-09 11:51:16 +1200

    Updated for pypy

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'pybleno',
   packages = ['pybleno', 'pybleno/hci_socket',  'pybleno/hci_socket/BluetoothHCI'], # this must be the same as the name above
-  version = '0.6',
+  version = '0.7',
   description = 'A direct port of the Bleno bluetooth LE peripheral role library to Python2/3',
   author = 'Adam Langley',
   author_email = 'github.com@irisdesign.co.nz',
   url = 'https://github.com/Adam-Langley/pybleno', # use the URL to the github repo
-  download_url = 'https://github.com/Adam-Langley/pybleno/archive/0.6.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/Adam-Langley/pybleno/archive/0.7.tar.gz', # I'll explain this in a second
   keywords = ['Bluetooth', 'Bluetooth Smart', 'BLE', 'Bluetooth Low Energy'], # arbitrary keywords
   classifiers=[
       'Programming Language :: Python :: 2.7',
