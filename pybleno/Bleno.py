23158fbc5a01992cab4734a0ca9e33644db163df
Dániel Buga <daniel.buga@revolutionrobotics.org>
2019-05-15 12:44:20 +0200

    Pass error object to callbacks, detect if callback is callable

--- pybleno/Bleno.py
+++ pybleno/Bleno.py
@@ -1,19 +1,33 @@
 import array
 import platform
+import sys
 from . import UuidUtil
 from .hci_socket import Emit
 
-platform = platform.system()
+platform = platform.system().lower()
 
 if platform == 'darwin':
     # import bindings = require('./mac/bindings');
     pass
-elif platform == 'Linux' or platform == 'Freebsd' or platform == 'Windows' or platform == 'Android':
+elif platform == 'linux' or platform == 'freebsd' or platform == 'windows' or platform == 'android':
     # bindings = require('./hci-socket/bindings');
     from .hci_socket import *
     # bindings = hci
 else:
     raise Exception('Unsupported platform')
+
+
+class Error(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+def is_callable(callback):
+    if sys.version_info[0] < 3 or sys.version_info[1] >= 2:
+        # Python 2.x or 3.2+
+        return callable(callback)
+    else:
+        return hasattr(callback, '__call__')
 
 
 class Bleno:
@@ -26,17 +40,17 @@ class Bleno:
 
         self._bindings = BlenoBindings()
 
-        self._bindings.on('stateChange', self.onStateChange);
-        self._bindings.on('platform', self.onPlatform);
-        self._bindings.on('addressChange', self.onAddressChange);
-        self._bindings.on('advertisingStart', self.onAdvertisingStart);
-        self._bindings.on('advertisingStop', self.onAdvertisingStop);
-        self._bindings.on('servicesSet', self.onServicesSet);
-        self._bindings.on('accept', self.onAccept);
-        self._bindings.on('mtuChange', self.onMtuChange);
-        self._bindings.on('disconnect', self.onDisconnect);
+        self._bindings.on('stateChange', self.onStateChange)
+        self._bindings.on('platform', self.onPlatform)
+        self._bindings.on('addressChange', self.onAddressChange)
+        self._bindings.on('advertisingStart', self.onAdvertisingStart)
+        self._bindings.on('advertisingStop', self.onAdvertisingStop)
+        self._bindings.on('servicesSet', self.onServicesSet)
+        self._bindings.on('accept', self.onAccept)
+        self._bindings.on('mtuChange', self.onMtuChange)
+        self._bindings.on('disconnect', self.onDisconnect)
 
-        self._bindings.on('rssiUpdate', self.onRssiUpdate);
+        self._bindings.on('rssiUpdate', self.onRssiUpdate)
 
     def start(self):
         self._bindings.init()
@@ -45,62 +59,56 @@ class Bleno:
         self.platform = platform
 
     def onStateChange(self, state):
-        self.state = state;
+        self.state = state
 
-        self.emit('stateChange', [state]);
+        self.emit('stateChange', [state])
 
     def onAddressChange(self, address):
         # debug('addressChange ' + address);
 
-        self.address = address;
+        self.address = address
 
     def onAccept(self, clientAddress):
         # debug('accept ' + clientAddress);
-        self.emit('accept', [clientAddress]);
+        self.emit('accept', [clientAddress])
 
     def onMtuChange(self, mtu):
         # debug('mtu ' + mtu);
 
-        self.mtu = mtu;
+        self.mtu = mtu
 
-        self.emit('mtuChange', [mtu]);
+        self.emit('mtuChange', [mtu])
 
     def onDisconnect(self, clientAddress):
         # debug('disconnect' + clientAddress);
-        self.emit('disconnect', [clientAddress]);
+        self.emit('disconnect', [clientAddress])
 
-    def startAdvertising(self, name, serviceUuids, callback=None):
-        if (self.state != 'poweredOn'):
-            # var error = new Error('Could not start advertising, state is ' + self.state + ' (not poweredOn)');
-
-            # if (typeof callback === 'function')
-            #callback(error)
-            callback(None)
-            # else
-            #    throw error;
+    def startAdvertising(self, name, service_uuids=None, callback=None):
+        if self.state != 'poweredOn':
+            error = Error('Could not start advertising, state is {0} (not poweredOn)'.format(self.state))
+            if is_callable(callback):
+                callback(error)
+            else:
+                raise error
 
         else:
-            if (callback):
+            if callback:
                 self.once('advertisingStart', [], callback)
 
-        undashedServiceUuids = []
-
-        if (serviceUuids and len(serviceUuids)):
-            for i in range(0, len(serviceUuids)):
-                undashedServiceUuids.append(UuidUtil.removeDashes(serviceUuids[i]));
+        if service_uuids is None:
+            service_uuids = []
+        undashedServiceUuids = map(UuidUtil.removeDashes, service_uuids)
 
         # print 'starting advertising %s %s' % (name, undashedServiceUuids)
-        self._bindings.startAdvertising(name, undashedServiceUuids);
+        self._bindings.startAdvertising(name, undashedServiceUuids)
 
     def startAdvertisingIBeacon(self, uuid, major, minor, measuredPower, callback=None):
-        if (self.state != 'poweredOn'):
-            # var error = new Error('Could not start advertising, state is ' + self.state + ' (not poweredOn)');
-
-            # if (typeof callback === 'function')
-            #callback(error)
-            callback(None)
-            # else
-            #    throw error;
+        if self.state != 'poweredOn':
+            error = Error('Could not start advertising, state is {0} (not poweredOn)'.format(self.state))
+            if is_callable(callback):
+                callback(error)
+            else:
+                raise error
 
         else:
             undashedUuid = UuidUtil.removeDashes(uuid)
@@ -115,7 +123,7 @@ class Bleno:
             writeUInt16BE(iBeaconData, minor, uuidDataLength + 2)
             writeInt8(iBeaconData, measuredPower, uuidDataLength + 4)
 
-            if (callback):
+            if callback:
                 self.once('advertisingStart', [], callback)
 
             # debug('iBeacon data = ' + iBeaconData.toString('hex'));
@@ -128,28 +136,26 @@ class Bleno:
             callback = scanData
             scanData = None
 
-        if (self.state != 'poweredOn'):
-            # var error = new Error('Could not advertising scanning, state is ' + self.state + ' (not poweredOn)');
-
-            # if (typeof callback === 'function')
-            #callback(error)
-            callback(None)
-            # else
-            #    throw error;
+        if self.state != 'poweredOn':
+            error = Error('Could not start advertising, state is {0} (not poweredOn)'.format(self.state))
+            if is_callable(callback):
+                callback(error)
+            else:
+                raise error
 
         else:
-            if (callback):
+            if callback:
                 self.once('advertisingStart', [], callback)
 
         # print 'starting advertising with EIR data %s %s' % (advertisementData, scanData)
-        self._bindings.startAdvertisingWithEIRData(advertisementData, scanData);
+        self._bindings.startAdvertisingWithEIRData(advertisementData, scanData)
 
     def onAdvertisingStart(self, error):
         # debug('advertisingStart: ' + error);
         if error:
             self.emit('advertisingStartError', [error])
 
-        self.emit('advertisingStart', [error]);
+        self.emit('advertisingStart', [error])
 
     def stopAdvertising(self, callback=None):
         if callback:
@@ -186,7 +192,7 @@ class Bleno:
         self._bindings.updateRssi()
 
     def onRssiUpdate(self, rssi):
-        self.emit('rssiUpdate', [rssi]);
+        self.emit('rssiUpdate', [rssi])
 
 
 Emit.Patch(Bleno)
