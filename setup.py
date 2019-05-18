7bb425c609ee3b113a885a782272370ad5022718
Adam-Langley <github.com@irisdesign.co.nz>
2019-05-18 13:41:38 +1200

    updated version for Pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'pybleno',
   packages = ['pybleno', 'pybleno/hci_socket',  'pybleno/hci_socket/BluetoothHCI'], # this must be the same as the name above
-  version = '0.9',
+  version = '0.11',
   description = 'A direct port of the Bleno bluetooth LE peripheral role library to Python2/3',
   author = 'Adam Langley',
   author_email = 'github.com@irisdesign.co.nz',
